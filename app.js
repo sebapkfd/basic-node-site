cad72aed693a3b4803ccd98ad3cfae108d1fcb78
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-02-06 20:28:16 -0300

    Refactored code with express

--- app.js
+++ app.js
@@ -1,37 +1,28 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express')
+const app = express();
+var path = require('path');
+const port = 8080;
 
-http.createServer(function (req, res) {
-  let q = url.parse(req.url, true);
-  let pagePath = q.pathname;
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
-  if(pagePath === '/' || pagePath.includes('index')) {
-    fs.readFile('index.html', (err, data) => {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data)
-      return res.end()
-    })
-  }
-  else if(pagePath.includes('about')) {
-    fs.readFile('about.html', (err, data) => {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data)
-      return res.end()
-    })
-  }
-  else if(pagePath.includes('contact')) {
-    fs.readFile('contact-me.html', (err, data) => {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data)
-      return res.end()
-    })
-  }
-  else {
-    fs.readFile('404.html', (err, data) => {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      res.write(data)
-      return res.end()
-    })
-  }
-}).listen(8080);
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/about.html', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/about.html'));
+});
+
+app.get('/contact-me.html', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/contact-me.html'));
+});
+
+app.use(function(req, res, next) {
+  res.status(404).sendFile(path.join(__dirname + '/public/404.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
